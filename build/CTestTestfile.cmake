3119cf16fb2ecbc4111e478cdfad951d02f96427
AlexandrosTheocharous <alextheocharous98@gmail.com>
2024-05-30 01:05:17 +0200

    init commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nathan/MDP/gmapping/src
+# Build directory: /home/nathan/MDP/gmapping/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot_gmapping")
---
+++ build/my_robot_gmapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nathan/MDP/gmapping/src/my_robot_gmapping
+# Build directory: /home/nathan/MDP/gmapping/build/my_robot_gmapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
